9760d60904c5fb96880651347dee5b1d3696393a
ben.meredith@gmail.com <ben.meredith@gmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-10 21:17:28 +0000

    copying trunk to new tag for v 2.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-old-post-date-remover/tags/2.2@1023384 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-old-post-date-remover.php
+++ wp-old-post-date-remover.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Plugin Name: WP Old Post Date Remover
- * Plugin URI: http://benandjacq.com
- * Description: Remove the date on posts and pages over 2 weeks old with no code necessary. 
- * Version: 1.0.1
+ * Plugin URI: http://benandjacq.com/wordpress-plugin-wp-old-post-date-remover/
+ * Description: Remove the date on older posts and pages with no code. You select the number of days! 
+ * Version: 2.2
  * Author: Ben Meredith
  * Author URI: http://benandjacq.com/
  * License: GPL2
@@ -24,15 +24,22 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+include 'OPDR-options.php';
+
 /* The Plugin Class */
 defined('ABSPATH') or die("No script kiddies please!");
+
+
+
 function wp_OPDR_is_Old_Enough ()
 	{
-		$today = date("r");
-		$articledate = get_the_time('r');
-		$difference = round((strtotime($today) - strtotime($articledate))/(24*60*60),0);
-
-            if ($difference >= 14) {
+	
+		$today = date( 'U' );
+		$articledate = get_the_time( 'U' );
+		$difference = round( ( $today - $articledate ) / ( 24*60*60 ),0);
+		$OPDRinput = get_option( 'opdr_days_to_show_date' );
+		            
+		        if ( $difference >= $OPDRinput && ( is_single() ) ) {
 			wp_enqueue_style( 'remove-style-meta', plugins_url( 'css/OPDRstyle.css', __FILE__ ), false, '1.0', 'all' );
 		} else {
 			
@@ -40,5 +47,24 @@ function wp_OPDR_is_Old_Enough ()
 	};
 add_action('loop_start', 'wp_OPDR_is_Old_Enough');	
 
+/* Adds link to settings from plugin list */
 
+function opdr_options_link($links) { 
+  $settings_link = '<a href="options-general.php?page=wp_old_post_date_remover">Settings</a>'; 
+  array_unshift( $links, $settings_link ); 
+  return $links; 
+}
+ 
+$plugin = plugin_basename(__FILE__); 
+add_filter("plugin_action_links_$plugin", 'opdr_options_link' );
 
+/* Uninstallation -- cleans up any mess we might have made. */
+
+function OPDR_on_uninstall()
+{
+	delete_option( 'opdr_days_to_show_date' );
+	
+	
+}
+
+register_uninstall_hook(    __FILE__, 'OPDR_on_uninstall' );
