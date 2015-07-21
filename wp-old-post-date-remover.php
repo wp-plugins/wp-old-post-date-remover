145af5c2f20604ad45864903aa481a7aa6001818
ben.meredith@gmail.com <ben.meredith@gmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-21 13:08:04 +0000

    update to 3.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-old-post-date-remover/tags/3.0@1203102 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-old-post-date-remover.php
+++ wp-old-post-date-remover.php
@@ -1,12 +1,14 @@
 <?php
 /**
  * Plugin Name: WP Old Post Date Remover
- * Plugin URI: http://benandjacq.com
- * Description: Remove the date on posts and pages over 2 weeks old with no code necessary. 
- * Version: 1.0.1
+ * Plugin URI: http://benandjacq.com/wordpress-plugin-wp-old-post-date-remover/
+ * Description: Remove the date on older posts and pages with no code. You select the number of days! 
+ * Version: 3.0
  * Author: Ben Meredith
  * Author URI: http://benandjacq.com/
  * License: GPL2
+ * Text Domain: wp-old-post-date-remover
+ * Domain Path: /languages
  */
  /*  Copyright 2014  Ben Meredith  (email : ben@benandjacq.com)
 
@@ -24,15 +26,22 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+include 'OPDR-options.php';
+
 /* The Plugin Class */
 defined('ABSPATH') or die("No script kiddies please!");
+
+
+
 function wp_OPDR_is_Old_Enough ()
 	{
-		$today = date("r");
-		$articledate = get_the_time('r');
-		$difference = round((strtotime($today) - strtotime($articledate))/(24*60*60),0);
-
-            if ($difference >= 14) {
+	
+		$today = date( 'U' );
+		$articledate = get_the_time( 'U' );
+		$difference = round( ( $today - $articledate ) / ( 24*60*60 ),0);
+		$OPDRinput = get_option( 'opdr_days_to_show_date' );
+		            
+		        if ( $difference >= $OPDRinput && ( is_single() ) ) {
 			wp_enqueue_style( 'remove-style-meta', plugins_url( 'css/OPDRstyle.css', __FILE__ ), false, '1.0', 'all' );
 		} else {
 			
@@ -40,5 +49,31 @@ function wp_OPDR_is_Old_Enough ()
 	};
 add_action('loop_start', 'wp_OPDR_is_Old_Enough');	
 
+/* Adds link to settings from plugin list */
 
+function opdr_options_link($links) { 
+  $settings_text = sprintf( _x( 'Settings', 'text for the link on the plugins page', 'wp-old-post-date-remover' ) );  
+  $settings_link = '<a href="options-general.php?page=wp_old_post_date_remover">' . $settings_text . '</a>';
+  array_unshift( $links, $settings_link ); 
+  return $links; 
+}
 
+$plugin = plugin_basename(__FILE__); 
+add_filter("plugin_action_links_$plugin", 'opdr_options_link' );
+
+/* Tell WordPress where to look for language files */
+function opdr_load_plugin_textdomain() {
+    load_plugin_textdomain( 'wp-old-post-date-remover', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'opdr_load_plugin_textdomain' );
+
+/* Uninstallation -- cleans up any mess we might have made. */
+
+function OPDR_on_uninstall()
+{
+	delete_option( 'opdr_days_to_show_date' );
+	
+	
+}
+
+register_uninstall_hook(    __FILE__, 'OPDR_on_uninstall' );
